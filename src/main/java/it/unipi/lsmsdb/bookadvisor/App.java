9f18cad7377812df5e7776ea2b5a4798355455df
Francesco <fralondretti@gmail.com>
2023-11-29 11:00:27 +0100

    Changed structure to match DAO paradigm

---
+++ src/main/java/it/unipi/lsmsdb/bookadvisor/App.java
@@ -0,0 +1,13 @@
+package it.unipi.lsmsdb.bookadvisor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
