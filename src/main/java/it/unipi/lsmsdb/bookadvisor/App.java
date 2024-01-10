bd7daf6f14bd194ed5257d00a67ca1a4aad7c033
Giulio Bello <giuliobello12@gmail.com>
2024-01-10 15:04:06 +0100

    Update MongoDB dependencies and fix MongoDB connection initialization

--- src/main/java/it/unipi/lsmsdb/bookadvisor/App.java
+++ src/main/java/it/unipi/lsmsdb/bookadvisor/App.java
@@ -1,5 +1,7 @@
 package it.unipi.lsmsdb.bookadvisor;
 
+import it.unipi.lsmsdb.bookadvisor.dao.documentDB.MongoDBConnector;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        MongoDBConnector.getInstance().getDatabase().getCollection("books");
     }
 }
