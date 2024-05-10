f258589e4776e89f091b52b2f43a4ac992e82f30
Froffri <fra.londretti@gmail.com>
2024-05-10 17:36:07 +0200

    done things

--- src/main/java/it/unipi/lsmsdb/bookadvisor/dao/documentDB/VoteDao.java
+++ src/main/java/it/unipi/lsmsdb/bookadvisor/dao/documentDB/VoteDao.java
@@ -1,7 +1,5 @@
 package it.unipi.lsmsdb.bookadvisor.dao.documentDB;
 
-import it.unipi.lsmsdb.bookadvisor.model.vote.Vote;
-
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
